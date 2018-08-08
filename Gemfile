bf719d5e807b631f7ec6443ac281ee3cc5a07941
Alexander Wisowaty <alexander@wisowaty.com>
2018-08-08 14:39:45 +0200

    added all functionality and styling

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.4.4'
 
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
 # Use sqlite3 as the database for Active Record
@@ -27,6 +28,11 @@ gem 'jbuilder', '~> 2.5'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
+
+gem 'bootstrap-sass'
+gem 'font-awesome-sass', '~> 5.0.9'
+gem 'simple_form'
+gem 'autoprefixer-rails'
 # Use ActiveStorage variant
 # gem 'mini_magick', '~> 4.8'
 
@@ -63,7 +69,3 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'rspec-rails', group: [ :test ]
 gem 'rails-controller-testing', group: [ :test ]
 
-gem 'bootstrap-sass'
-gem 'font-awesome-sass', '~> 5.0.9'
-gem 'simple_form'
-gem 'autoprefixer-rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,43 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+CATEGORIES = ["chinese", "italian", "japanese", "french", "belgian"]
+
+
+restaurants_attributes = [
+  {
+    name:         Faker::Restaurant.name,
+    address:      '100 Main St',
+    phone_number: '111 222 3333',
+    category:     CATEGORIES.sample
+  },
+  {
+    name:         Faker::Restaurant.name,
+    address:      '100 Main St',
+    phone_number: '111 222 3333',
+    category:     CATEGORIES.sample
+  },
+  {
+    name:         Faker::Restaurant.name,
+    address:      '100 Main St',
+    phone_number: '111 222 3333',
+    category:     CATEGORIES.sample
+  },
+  {
+    name:         Faker::Restaurant.name,
+    address:      '100 Main St',
+    phone_number: '111 222 3333',
+    category:     CATEGORIES.sample
+  },
+  {
+    name:         Faker::Restaurant.name,
+    address:      '100 Main St',
+    phone_number: '111 222 3333',
+    category:     CATEGORIES.sample
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
